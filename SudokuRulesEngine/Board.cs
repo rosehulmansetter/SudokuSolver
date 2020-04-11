160cd873715efb56d87826bb82d9728cb2c348a5
Matt Gold <matthew.t.gold@gmail.com>
2020-04-11 18:24:34 -0400

    Fixing some minor UI things

--- SudokuRulesEngine/Board.cs
+++ SudokuRulesEngine/Board.cs
@@ -37,17 +37,17 @@ namespace SudokuRulesEngine
             return cellData.All(c => c.Count == 1);
         }
 
-        internal CellData GetCellDataForSquare(int i)
+        public CellData GetCellDataForSquare(int i)
         {
             return GetCellDataForIndices(GridMath.GetIndicesInSquare(i));
         }
 
-        internal CellData GetCellDataForColumn(int i)
+        public CellData GetCellDataForColumn(int i)
         {
             return GetCellDataForIndices(GridMath.GetIndicesInColumn(i));
         }
 
-        internal CellData GetCellDataForRow(int i)
+        public CellData GetCellDataForRow(int i)
         {
             return GetCellDataForIndices(GridMath.GetIndicesInRow(i));
         }
@@ -65,42 +65,6 @@ namespace SudokuRulesEngine
         public List<List<int>> GetCellData()
         {
             return cellData;
-        }
-
-        public override bool Equals(object obj)
-        {
-            var board = obj as Board;
-            return board != null &&
-                   EqualityComparer<List<List<int>>>.Default.Equals(cellData, board.cellData);
-        }
-
-        public override int GetHashCode()
-        {
-            return 334095517 + EqualityComparer<List<List<int>>>.Default.GetHashCode(cellData);
-        }
-
-        public static bool operator ==(Board board1, Board board2)
-        {
-            List<List<int>> cellData1 = board1.GetCellData();
-            List<List<int>> cellData2 = board2.GetCellData();
-            for(int i = 0; i < cellData1.Count; i++)
-            {
-                List<int> possibleNumbers1 = cellData1[i];
-                List<int> possibleNumbers2 = cellData2[i];
-
-                if (possibleNumbers1.Count != possibleNumbers2.Count || 
-                    possibleNumbers1.Intersect(possibleNumbers2).Count() != possibleNumbers1.Count)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public static bool operator !=(Board board1, Board board2)
-        {
-            return !(board1 == board2);
         }
     }
 }
